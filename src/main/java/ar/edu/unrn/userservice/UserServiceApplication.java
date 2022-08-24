f94aca8dffa26682a54252ce07fc278aa2322722
rodrigoArceUnrn <106389494+rodrigoArceUnrn@users.noreply.github.com>
2022-08-24 19:49:08 -0300

    first commit

---
+++ src/main/java/ar/edu/unrn/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unrn.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}

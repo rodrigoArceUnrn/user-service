532125ef324f36e776356e2249182f4b0129c341
Rodrigo <arcerodrigo3@gmail.com>
2022-11-23 13:16:43 -0300

    merged

--- src/main/java/ar/edu/unrn/userservice/service/impl/ClientServiceImpl.java
+++ src/main/java/ar/edu/unrn/userservice/service/impl/ClientServiceImpl.java
@@ -2,9 +2,9 @@ package ar.edu.unrn.userservice.service.impl;
 
 import ar.edu.unrn.userservice.dto.ClientDTO;
 import ar.edu.unrn.userservice.exception.ClientException;
-import ar.edu.unrn.userservice.model.Client;
 import ar.edu.unrn.userservice.rabbitmq.Producer;
 import ar.edu.unrn.userservice.repository.ClientRepository;
+import ar.edu.unrn.userservice.security.entity.Client;
 import ar.edu.unrn.userservice.service.ClientService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
